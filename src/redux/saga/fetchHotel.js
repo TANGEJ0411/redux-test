adbddec5da68f564b79538a5e0b975d2bc0c12d6
唐翊傑 <tangej@tangyijiedeMacBook-Pro.local>
2023-10-27 11:17:15 +0800

    加入saga的註解

--- src/redux/saga/fetchHotel.js
+++ src/redux/saga/fetchHotel.js
@@ -7,10 +7,12 @@ const FETCH_HOTEL_SUCCESS = 'FETCH_HOTEL_SUCCESS'
 
 const FETCH_HOTEL_FAILED = 'FETCH_HOTEL_FAILED'
 
-
+// 此為generator function 能夠配合yield在執行過程中暫停
 function* fetchData() {
     try {
+        // call( function, arg1, arg2... )第一個傳入要執行的function，後面傳入該function要使用的參數
         const data = yield call(axios.get('https://raw.githubusercontent.com/TANGEJ0411/fakeDB/main/db.json'))
+        // put 相當於dispatch概念可以用來觸發store 執行reducer
         yield put({ type: FETCH_HOTEL_SUCCESS, payload: { data } });
     } catch (error) {
         yield put({ type: FETCH_HOTEL_FAILED, payload: { data: error.message } });
